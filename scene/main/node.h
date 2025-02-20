6ca9d7eb44ce0f3733c1ea86e457f2efbd34df04
Jānis Kiršteins <janis.kirsteins@icloud.com>
2025-02-20 23:48:43 +0100

    Add translation_context to Control nodes

--- scene/main/node.h
+++ scene/main/node.h
@@ -378,6 +378,9 @@ protected:
 
 	void _validate_property(PropertyInfo &p_property) const;
 
+	// Localization.
+	virtual String _get_translation_context_with_override(const StringName &p_context) const { return p_context; }
+
 protected:
 	virtual void input(const Ref<InputEvent> &p_event);
 	virtual void shortcut_input(const Ref<InputEvent> &p_key_event);
@@ -753,10 +756,10 @@ public:
 	virtual void set_translation_domain(const StringName &p_domain) override;
 	void set_translation_domain_inherited();
 
-	_FORCE_INLINE_ String atr(const String &p_message, const StringName &p_context = "") const { return can_auto_translate() ? tr(p_message, p_context) : p_message; }
+	_FORCE_INLINE_ String atr(const String &p_message, const StringName &p_context = "") const { return can_auto_translate() ? tr(p_message, _get_translation_context_with_override(p_context)) : p_message; }
 	_FORCE_INLINE_ String atr_n(const String &p_message, const StringName &p_message_plural, int p_n, const StringName &p_context = "") const {
 		if (can_auto_translate()) {
-			return tr_n(p_message, p_message_plural, p_n, p_context);
+			return tr_n(p_message, p_message_plural, p_n, _get_translation_context_with_override(p_context));
 		}
 		return p_n == 1 ? p_message : String(p_message_plural);
 	}
